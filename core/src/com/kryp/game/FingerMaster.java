493b48e57531be9e73b05280254aac0b2ec1af99
Nanthawan <nanthawan.han@ku.th>
2017-11-10 15:02:08 +0700

    blank window

---
+++ core/src/com/kryp/game/FingerMaster.java
@@ -0,0 +1,26 @@
+package com.kryp.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class FingerMaster extends ApplicationAdapter {
+	SpriteBatch batch;
+	 
+    @Override
+    public void create () {
+        batch = new SpriteBatch();
+    }
+ 
+    @Override
+    public void render () {
+        super.render();
+    }
+ 
+    @Override
+    public void dispose () {
+        batch.dispose();
+    }
+}
