5d06a2ed438284ee6b274b6da9731e14aa669bc6
Nanthawan <nanthawan.han@ku.th>
2017-12-01 04:50:53 +0700

    A for Player1 L for Player2

--- core/src/com/kryp/game/FingerMaster.java
+++ core/src/com/kryp/game/FingerMaster.java
@@ -1,17 +1,15 @@
 package com.kryp.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class FingerMaster extends ApplicationAdapter {
+public class FingerMaster extends Game {
 	SpriteBatch batch;
 	 
     @Override
     public void create () {
         batch = new SpriteBatch();
+        setScreen(new GameScreen(this));
     }
  
     @Override
